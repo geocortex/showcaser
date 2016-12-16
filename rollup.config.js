e3de841768955e06b55ae008b28c990c46a35f12
Ian Schmitz <ianschmitz@gmail.com>
2016-12-15 17:44:29 -0800

    Add uglify-js to build process

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     entry: 'build/showcaser.js',
-    dest: 'dist/showcaser.min.js',
+    dest: 'dist/showcaser.js',
     format: 'iife',
     moduleName: 'Showcaser',
     plugins: [
