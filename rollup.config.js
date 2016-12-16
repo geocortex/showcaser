f2436897eb55675a85a6c62b2a2ee86b521b4788
Ian Schmitz <ianschmitz@gmail.com>
2016-12-15 19:32:15 -0800

    Add PostCSS to rollup build to include external CSS styles in bundled Javascript output

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,25 @@
+// Helpful series on configuring rollup: https://code.lengstorf.com/learn-rollup-js/
+
+// Rollup plugins
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+
+// PostCSS plugins
+import cssnano from 'cssnano';
 
 export default {
-    entry: 'build/showcaser.js',
+    entry: 'src/showcaser.js',
     dest: 'dist/showcaser.js',
     format: 'iife',
     moduleName: 'Showcaser',
     plugins: [
+        postcss({
+            plugins: [
+                cssnano()
+            ],
+            extensions: ['.css']
+        }),
         nodeResolve({
             jsnext: true,
             main: true,
